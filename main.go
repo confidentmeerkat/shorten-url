d5eff6d4d47d1b8b02dbb9f02bd9ed0e51bc4960
confidentmeerkat <confidentmeerkat@gmail.com>
2023-02-18 22:56:40 +0330

    basic log for web server

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from Web!\n")
-	})
+	http.HandleFunc("/", hello)
 
-	http.ListenAndServe(":8080", nil)
+	log.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from Web!\n")
 }
