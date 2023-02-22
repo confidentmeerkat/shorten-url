ee23db72c177fbd07ec378235460894beb546455
confidentmeerkat <confidentmeerkat@gmail.com>
2023-02-22 21:56:07 +0330

    Changed public folder name to view

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.Handle("/", http.FileServer(http.Dir("./view")))
 
 	log.Println("Server is running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
