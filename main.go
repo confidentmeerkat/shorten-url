4842b43508b4ec8f53625d5fc0de446f3b0fd84f
confidentmeerkat <confidentmeerkat@gmail.com>
2023-03-20 17:57:24 +0330

    better resource serving

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -14,49 +13,43 @@ func main() {
 	http.HandleFunc("/", controller.Handler)
 	http.HandleFunc("/short", controller.Short)
 
-	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
-	// http.HandleFunc("/public/", serveResource)
-	// http.HandleFunc("/public/", resouece)
+	// http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	http.HandleFunc("/public/", serveResource)
 
 	log.Println("Server is running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
 
-func resouece(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	if strings.HasSuffix(path, ".css") {
-		http.FileServer(http.Dir("public/css"))
-		// buffer := bufio.NewReader(h)
-		// buffer.WriteTo(w)
-	}
-}
-
 func serveResource(w http.ResponseWriter, req *http.Request) {
-	fmt.Println(req.URL.Path)
-	fmt.Println(req.URL.RawPath)
+	// fmt.Println(req.URL.Path)
+	// fmt.Println(req.URL.RawPath)
 
 	// path := "public" + req.URL.Path
 	path := req.URL.Path
+	path, _ = strings.CutPrefix(path, "/")
 
 	var contentType string
 	if strings.HasSuffix(path, ".css") {
-		path = "public/css/"
+		// path = "public/css/"
 		contentType = "text/css"
-		fmt.Println(contentType)
+		// fmt.Println(contentType)
 	} else if strings.HasSuffix(path, ".png") {
 		contentType = "image/png"
-		fmt.Println(contentType)
+		// fmt.Println(contentType)
+	} else if strings.HasSuffix(path, ".jpg") {
+		contentType = "image/jpg"
+		// fmt.Println(contentType)
 	} else if strings.HasSuffix(path, ".js") {
 		contentType = "text/javascript"
-		fmt.Println(contentType)
+		// fmt.Println(contentType)
 	} else {
 		contentType = "text/plain"
-		fmt.Println(contentType)
+		// fmt.Println(contentType)
 	}
-	fmt.Println(path)
-	o, _ := os.Getwd()
-	fmt.Println(o, path)
+	// fmt.Println(path)
+	// o, _ := os.Getwd()
+	// fmt.Println(o, path)
 	f, err := os.Open(path)
 
 	if err == nil {
