19ece1630281f27555ac1450dc25ab8d5f16b55a
confidentmeerkat <confidentmeerkat@gmail.com>
2023-03-27 05:08:50 +0330

    Removed commented code

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ func main() {
 	http.HandleFunc("/", controller.Handler)
 	http.HandleFunc("/short", controller.Short)
 
-	// http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	http.HandleFunc("/public/", serveResource)
 
 	http.HandleFunc("/api/get", api.GetShort)
@@ -26,34 +25,22 @@ func main() {
 }
 
 func serveResource(w http.ResponseWriter, req *http.Request) {
-	// fmt.Println(req.URL.Path)
-	// fmt.Println(req.URL.RawPath)
-
-	// path := "public" + req.URL.Path
 	path := req.URL.Path
 	path, _ = strings.CutPrefix(path, "/")
 
 	var contentType string
 	if strings.HasSuffix(path, ".css") {
-		// path = "public/css/"
 		contentType = "text/css"
-		// fmt.Println(contentType)
 	} else if strings.HasSuffix(path, ".png") {
 		contentType = "image/png"
-		// fmt.Println(contentType)
 	} else if strings.HasSuffix(path, ".jpg") {
 		contentType = "image/jpg"
-		// fmt.Println(contentType)
 	} else if strings.HasSuffix(path, ".js") {
 		contentType = "text/javascript"
-		// fmt.Println(contentType)
 	} else {
 		contentType = "text/plain"
-		// fmt.Println(contentType)
 	}
-	// fmt.Println(path)
-	// o, _ := os.Getwd()
-	// fmt.Println(o, path)
+
 	f, err := os.Open(path)
 
 	if err == nil {
