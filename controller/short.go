343e65fce210c009f0b97cbb0b73f57f4f8b01fd
confidentmeerkat <confidentmeerkat@gmail.com>
2023-03-27 05:37:56 +0330

    Added method handling

--- controller/short.go
+++ controller/short.go
@@ -12,52 +12,56 @@ import (
 // var SHORTLINK map[string]string
 
 func Short(w http.ResponseWriter, r *http.Request) {
-	token := r.FormValue("token")
-	cToken, err := r.Cookie("token")
-	if err != nil {
-		w.WriteHeader(http.StatusFound)
-	}
-
-	fmt.Println("token: ", token)
-	fmt.Println("cookie: ", cToken.Value)
-	if cToken.Value == token {
-		u := template.HTMLEscapeString(r.FormValue("url"))
-		_, err := url.ParseRequestURI(u)
+	if r.Method == http.MethodPost {
+		token := r.FormValue("token")
+		cToken, err := r.Cookie("token")
 		if err != nil {
-			log.Fatal(err)
+			w.WriteHeader(http.StatusFound)
 		}
 
-		fmt.Println(u)
+		fmt.Println("token: ", token)
+		fmt.Println("cookie: ", cToken.Value)
+		if cToken.Value == token {
+			u := template.HTMLEscapeString(r.FormValue("url"))
+			_, err := url.ParseRequestURI(u)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		db, err := postgres.New()
-		if err != nil {
-			w.WriteHeader(503)
+			fmt.Println(u)
 
-			log.Fatal(err)
-		}
-		fmt.Println("connected")
-
-		short, err := db.GetShort(u)
-		if err != nil {
-			short, err = db.CreateShort(u, 4)
+			db, err := postgres.New()
 			if err != nil {
 				w.WriteHeader(503)
 
 				log.Fatal(err)
 			}
+			fmt.Println("connected")
+
+			short, err := db.GetShort(u)
+			if err != nil {
+				short, err = db.CreateShort(u, 4)
+				if err != nil {
+					w.WriteHeader(503)
+
+					log.Fatal(err)
+				}
+			}
+
+			var cookie http.Cookie
+
+			cookie.Name = "shortLink"
+			cookie.Value = short
+
+			fmt.Println("right token")
+			http.SetCookie(w, &cookie)
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
+			fmt.Println("wrong token")
+			http.Redirect(w, r, "/", http.StatusFound)
 		}
-
-		var cookie http.Cookie
-
-		cookie.Name = "shortLink"
-		cookie.Value = short
-
-		fmt.Println("right token")
-		http.SetCookie(w, &cookie)
-		http.Redirect(w, r, "/", http.StatusFound)
 	} else {
-		fmt.Println("wrong token")
-		http.Redirect(w, r, "/", http.StatusFound)
+		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 
 }
